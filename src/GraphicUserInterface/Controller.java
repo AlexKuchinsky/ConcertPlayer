90985d0f00b46fe3c9ef31a379616d99ee4c4e22
AlexKuchinsky <alexkuchinskydev@gmail.com>
2017-05-07 00:54:51 +0300

    adding MenueItems with Image View

--- src/GraphicUserInterface/Controller.java
+++ src/GraphicUserInterface/Controller.java
@@ -3,30 +3,58 @@ package GraphicUserInterface;
 import MediaComponents.Song;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import java.io.*;
 import java.net.URL;
+import java.util.Comparator;
 import java.util.ResourceBundle;
+import java.util.TreeSet;
 
 public class Controller implements Initializable{
     @FXML
+    public Menu menu;
+    @FXML
+    public Button button;
+    @FXML
+    public TextField textArea;
+    @FXML
+    public MenuBar menuBar;
+
+    public void addFunction(){
+
+    }
+    /*@FXML
     private Button playButton;
     @FXML
     private Label informationLabel;
-    @Override
+    @Override*/
     public void initialize(URL location, ResourceBundle resources){
-        System.out.println(" Initialize method");
+        button.setOnAction(e->{
+            Image image=new Image("file:/"+"C:/Users/Alex/IdeaProjects/ConcertPlayer/src/photo.jpg");
+
+            ImageView imageView =new ImageView(image);
+            imageView.setFitHeight(40);
+            imageView.setFitWidth(40);
+            MenuItem menuItem=new MenuItem(textArea.getText(),imageView);
+            menuItem.setOnAction(b->{
+                System.out.println(textArea.getText() + "menue was choosen");
+            });
+            menu.getItems().add(menuItem);
+        });
+
+       /* System.out.println(" Initialize method");
         playButton.setOnAction(action->{
             mediaPlayer=new MediaPlayer(song.getSong());
             mediaPlayer.play();
             informationLabel.setText(song.getNameOfTheSong());
-        });
+        });*/
     }
-    private static MediaPlayer mediaPlayer;
+   /* private static MediaPlayer mediaPlayer;
     private static Song song;
     public Controller(){
         System.out.println(" Contorller constructor");
@@ -47,9 +75,9 @@ public class Controller implements Initializable{
         MediaPlayer mediaPlayer=new MediaPlayer(media);
         mediaPlayer.play();*/
         /// You are my boss
-    }
+  /*  }
     public void play(){
         System.out.println(" play ");
 
-    }
+    }*/
 }
